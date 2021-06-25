8d2c12a51c538ed3edf6f0ab1fd6196f5eb4c08f
Dorothea Sarri <dorotheasarri95@gmail.com>
2021-06-25 19:10:08 +0300

    Create classes Customer, Item and Employee

--- FuelStationApp/Impl/Customer.cs
+++ FuelStationApp/Impl/Customer.cs
@@ -5,6 +5,23 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace FuelStationApp.Impl {
-    class Customer {
+
+
+    public class Customer {
+        public Guid ID { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public int CardNumber { get; set; }
+      
+        public Customer() { }
+        public Customer(string name, string surname, int cardnumber) {
+
+            ID = Guid.NewGuid();
+            Name = name;
+            Surname = surname;
+            CardNumber = cardnumber;
+          
+
+        }
     }
 }
---
+++ FuelStationApp/Impl/Employee.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FuelStationApp.Impl {
+    class Employee {
+        public Guid ID { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public decimal Salary { get; set; }
+        public DateTime DateStart { get; set; }
+        public DateTime DateEnd { get; set; }
+
+        public Employee() {
+
+        }
+        public Employee(string name, string surname, decimal salary,  DateTime datestart, DateTime dateend) {
+
+            ID = Guid.NewGuid();
+            Name = name;
+            Surname = surname;
+            Salary = salary;
+            DateStart = datestart;
+            DateEnd = dateend;
+        }
+    }
+}
