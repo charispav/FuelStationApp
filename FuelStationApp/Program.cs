42f76a4e3886fe5f305eddedc7f2081cc6278fe8
Charalampos Pavlidis <pavlidis.charis@gmail.com>
2021-06-25 17:26:25 +0300

    comment

--- FuelStationApp/Program.cs
+++ FuelStationApp/Program.cs
@@ -16,6 +16,7 @@ namespace FuelStationApp {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new MainForm());
+            //change
         }
     }
 }
